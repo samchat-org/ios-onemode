c434f3a2aba1d153b4f50a4b368b1051a030776c
gknows <gknows@qq.com>
2016-11-30 13:07:17 +0800

    add test feature upload log to AWSS3

---
+++ SamChat/Classes/Sections/Setting/Log/SAMCLogUploader.h
@@ -0,0 +1,13 @@
+//
+//  SAMCLogUploader.h
+//  SamChat
+//
+//  Created by HJ on 11/30/16.
+//  Copyright © 2016 SamChat. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SAMCLogUploader : NSObject
+
+@end
---
+++ SamChat/Classes/Sections/Setting/Log/SAMCLogUploader.m
@@ -0,0 +1,13 @@
+//
+//  SAMCLogUploader.m
+//  SamChat
+//
+//  Created by HJ on 11/30/16.
+//  Copyright © 2016 SamChat. All rights reserved.
+//
+
+#import "SAMCLogUploader.h"
+
+@implementation SAMCLogUploader
+
+@end
